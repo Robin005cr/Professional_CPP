d503b41bc4935977cb67a5251dfd0f79239bd416
Robin CR <robin005cr@gmail.com>
2025-01-14 23:00:11 +0530

    Adding the author and contact

--- Constructor/ConstructorOutsideClass.cpp
+++ Constructor/ConstructorOutsideClass.cpp
@@ -1,36 +1,49 @@
-	#include <iostream>
-	using namespace std;
-	class student {
-	    int rno;
-	    char name[50];
-	    double fee;
+/*
+* project   : https://github.com/Robin005cr/Professional_CPP
+* file name : ConstructorOutsideClass.cpp
+* author    : Robin CR
+* mail id   : robinchovallurraju@gmail.com
+* LinkedIn  : https://www.linkedin.com/in/robin-cr/
+* portfolio : https://robin005cr.github.io/
+*
+* Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+* Suggestions for improvements or better methods are always welcome and appreciated.
+* I value constructive feedback and aim to continuously improve the quality of the work.
+*
+*/
+#include <iostream>
+using namespace std;
+class student {
+	int rno;
+	char name[50];
+	double fee;
 	 
-	public:
-	    student();
-	    void display();
-	};
+public:
+	student();
+	void display();
+};
 	 
-	student::student()
-	{
-	    cout << "Enter the RollNo:";
-	    cin >> rno;
+student::student()
+{
+	cout << "Enter the RollNo:";
+	cin >> rno;
 	 
-	    cout << "Enter the Name:";
-	    cin >> name;
+	cout << "Enter the Name:";
+	cin >> name;
 	 
-	    cout << "Enter the Fee:";
-	    cin >> fee;
-	}
+	cout << "Enter the Fee:";
+	cin >> fee;
+}
 	 
-	void student::display()
-	{
-	    cout << endl << rno << "\t" << name << "\t" << fee;
-	}
+void student::display()
+{
+	cout << endl << rno << "\t" << name << "\t" << fee;
+}
 	 
-	int main()
-	{
-	    student s;
-	    s.display();
+int main()
+{
+	student s;
+	s.display();
 	 
-	    return 0;
-	}
+	return 0;
+}
--- Constructor/ConstructorWithinClass.cpp
+++ Constructor/ConstructorWithinClass.cpp
@@ -1,34 +1,47 @@
-	#include <iostream>
-	using namespace std;
+/*
+* project   : https://github.com/Robin005cr/Professional_CPP
+* file name : ConstructorWithinClass.cpp
+* author    : Robin CR
+* mail id   : robinchovallurraju@gmail.com
+* LinkedIn  : https://www.linkedin.com/in/robin-cr/
+* portfolio : https://robin005cr.github.io/
+*
+* Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+* Suggestions for improvements or better methods are always welcome and appreciated.
+* I value constructive feedback and aim to continuously improve the quality of the work.
+*
+*/
+#include <iostream>
+using namespace std;
 	 
-	class student {
-	    int rno;
-	    char name[10];
-	    double fee;
+class student {
+	int rno;
+	char name[10];
+	double fee;
 	 
-	public:
-	    student()
-	    {
-	        cout << "Enter the RollNo:";
-	        cin >> rno;
-	        cout << "Enter the Name:";
-	        cin >> name;
-	        cout << "Enter the Fee:";
-	        cin >> fee;
-	    }
-	 
-	    void display()
-	    {
-	        cout << endl << rno << "\t" << name << "\t" << fee;
-	    }
-	};
-	 
-	int main()
+public:
+	student()
 	{
-	    student s; // constructor gets called automatically when
-	               // we create the object of the class
-	    s.display();
-	 
-	    return 0;
+	    cout << "Enter the RollNo:";
+	    cin >> rno;
+	    cout << "Enter the Name:";
+	    cin >> name;
+	    cout << "Enter the Fee:";
+	    cin >> fee;
 	}
+	 
+	void display()
+	{
+	    cout << endl << rno << "\t" << name << "\t" << fee;
+	}
+};
+	 
+int main()
+{
+	student s; // constructor gets called automatically when
+	            // we create the object of the class
+	s.display();
+	 
+	return 0;
+}
 
