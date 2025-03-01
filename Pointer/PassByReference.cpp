743f4b03c1c66c9c585a37e8df40905d87c72750
Robin CR <robin005cr@gmail.com>
2025-03-02 00:48:26 +0530

    Pointer folder structure created

---
+++ Pointer/PassByReference.cpp
@@ -0,0 +1,17 @@
+// Pass by reference
+#include <iostream>
+using namespace std;
+
+void update(int &var)
+{
+    var = var+10;
+}
+int main()
+{
+    int x = 10;
+    cout<<"Before Function call X :"<<x<<endl;
+    update(x);
+    cout<<"After Function call X :"<<x<<endl;
+
+    return 0;
+}
---
+++ Pointer/PassByValue.cpp
@@ -0,0 +1,18 @@
+// Pass by value
+
+#include <iostream>
+using namespace std;
+
+void update(int var)
+{
+    var = var+10;
+}
+int main()
+{
+    int x = 10;
+    cout<<"Before Function call X :"<<x<<endl;
+    update(x);
+    cout<<"After Function call X :"<<x<<endl;
+
+    return 0;
+}
