cd75184c4f63589d8dc38b8dd1836028ac7869a0
Robin CR <robin005cr@gmail.com>
2025-04-15 19:43:23 +0530

    Adapting the comments

--- Pointer/smart_pointers/unique_ptr/unique_ptr_dealloc_curly_brace.cpp
+++ Pointer/smart_pointers/unique_ptr/unique_ptr_dealloc_curly_brace.cpp
@@ -1,5 +1,16 @@
-Example 4.2 : class with curly braces - automatically deallocation
-
+/*
+* project   : https://github.com/Robin005cr/Professional_CPP
+* file name : unique_ptr_dealloc_curly_brace.cpp
+* author    : Robin CR
+* mail id   : robinchovallurraju@gmail.com
+* LinkedIn  : https://www.linkedin.com/in/robin-cr/
+* portfolio : https://robin005cr.github.io/
+*
+* Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+* Suggestions for improvements or better methods are always welcome and appreciated.
+* I value constructive feedback and aim to continuously improve the quality of the work.
+*
+*/
 #include <iostream>
 #include <memory>
 using namespace std;
