7106bb59b851a3cd6831ba107b95f46ad77164a3
Robin CR <robin005cr@gmail.com>
2025-08-21 23:15:52 +0530

    friend keyword - function

---
+++ Friend/friendFun.cpp
@@ -0,0 +1,50 @@
+/*
+ * project   : https://github.com/Robin005cr/Professional_CPP
+ * file name : friendFun.cpp
+ * author    : Robin CR
+ * mail id   : robinchovallurraju@gmail.com
+ * LinkedIn  : https://www.linkedin.com/in/robin-cr/
+ * portfolio : https://robin005cr.github.io/
+ *
+ * Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+ * Suggestions for improvements or better methods are always welcome and appreciated.
+ * I value constructive feedback and aim to continuously improve the quality of the work.
+ *
+ */
+#include <iostream>
+using namespace std;
+
+class base
+{
+private:
+    int private_variable;
+
+protected:
+    int protected_variable;
+
+public:
+    base()
+    {
+        private_variable = 10;
+        protected_variable = 99;
+    }
+
+    // friend function declaration
+    friend void friendFunction(base &obj);
+};
+
+// friend function definition
+void friendFunction(base &obj)
+{
+    cout << "Private Variable: " << obj.private_variable
+         << endl;
+    cout << "Protected Variable: " << obj.protected_variable;
+}
+
+int main()
+{
+    base object1;
+    friendFunction(object1);
+
+    return 0;
+}
