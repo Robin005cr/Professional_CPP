07036c1e76d2d2b1dcc5fd5725870f743e49d20c
Robin CR <robin005cr@gmail.com>
2025-09-30 00:19:59 +0530

    explicit keyword problem and solution

---
+++ explicit_keyword/explicit_problem.cpp
@@ -0,0 +1,32 @@
+/*
+* project   : https://github.com/Robin005cr/Professional_CPP
+* file name : explicit_problem.cpp
+* author    : Robin CR
+* mail id   : robinchovallurraju@gmail.com
+* LinkedIn  : https://www.linkedin.com/in/robin-cr/
+* portfolio : https://robin005cr.github.io/
+*
+* Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+* Suggestions for improvements or better methods are always welcome and appreciated.
+* I value constructive feedback and aim to continuously improve the quality of the work.
+*
+*/
+#include <iostream>
+using namespace std;
+
+class Meter {
+    int m;
+public:
+    // single-argument constructor
+    Meter(int x) : m(x) {}
+    int get() { return m; }
+};
+
+void printMeter(Meter obj) {
+    cout << "Value in meters = " << obj.get() << endl;
+}
+
+int main() {
+    printMeter(5);   // ⚠ Works! int -> Meter implicitly
+    return 0;
+}
