30eb9fa1ccd54fae10b77c807d0236c6bd5862e0
Robin CR <robin005cr@gmail.com>
2026-01-16 23:46:14 +0530

    restructured template programming folder

--- Template_Programming/class/FullTempSpecialization.cpp
+++ Template_Programming/class/FullTempSpecialization.cpp
@@ -1,3 +1,16 @@
+/*
+* project   : https://github.com/Robin005cr/Professional_CPP
+* file name : FullTempSpecialization.cpp
+* author    : Robin CR
+* mail id   : robinchovallurraju@gmail.com
+* LinkedIn  : https://www.linkedin.com/in/robin-cr/
+* portfolio : https://robin005cr.github.io/
+*
+* Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+* Suggestions for improvements or better methods are always welcome and appreciated.
+* I value constructive feedback and aim to continuously improve the quality of the work.
+*
+*/
 #include <iostream>
 #include <string>
 using namespace std;
@@ -21,7 +34,7 @@ template<> string add<string>(string num1, string num2)
 
 int main() {
     cout << "Hello World" << endl;
-    cout << add<int>(5, 6) << endl;  // Uncomment if needed, adds integers
+    cout << add<int>(5, 6) << endl;  
     cout << static_cast<int>(add('2', '3')) << endl;  // Adds chars ('2' + '3' => 2 + 3 => 5)
     cout << add<string>("How", "are you?") << endl;  // Concatenates strings ("How are you?")
     return 0;
